0142dbca04e3bb53fc81a5f69d7fda3fc9e7fb68
Nicolás Lasso <lasso.nico@gmail.com>
2015-06-07 17:20:47 -0300

    Traceroute terminado

--- tp2/main.py
+++ tp2/main.py
@@ -6,41 +6,22 @@ import pickle
 from scapy.all import *
 
 def main():
-
-	timeToLive = 50
+	timeToLive = 1
 	target = "www.facebook.com"
 
-	for x in range(1,timeToLive):
+	while True:
+		pass
 		icmp = ICMP()
 		icmp.type=8
-		pkt = IP(dst=target, ttl=x)/icmp/"XXXX"
-		icmpPacket = sr1(pkt, verbose=0)
-		if (icmpPacket[ICMP].type == 11):
-			print("Time Exceeded en la IP: ", icmpPacket[IP].src)
-		else:
-			print("Llegaste hasta: ", target)
-		#Esto fue de prueba, sino les anda pruebenlo igual entiendo que si está en el scope del FOR no debería importar.
-		#del(pkt.getlayer(IP).chksum) 
+		pkt = IP(dst=target, ttl=timeToLive)/icmp/"XXXX"
+		icmpPacket = sr1(pkt, verbose=0, timeout=2)
+		if (not (icmpPacket is None)):
+			if (icmpPacket[ICMP].type == 11):
+				print("Time Exceeded en la IP: ", icmpPacket[IP].src)
+			else:
+				print("Llegaste hasta", target, " que tiene la IP: ", icmpPacket[IP].src)
+				break
+		timeToLive += 1
 
 if __name__ == "__main__":
 	main()
-
-#ip = IP(dst=target, ttl=timeToLive)
-##traceroute(["www.google.com"], maxttl=20)
-#icmpPacket = sr1(ip/icmp/"XXXXXXXX")
-#icmpPacket[0].display()
-#print(icmpPacket[IP].src)
-##icmpPacket.show()
-##print(icmpPacket[ICMP].src)
-##print(icmpPacket[ICMP].type)
-#timeToLive += 1
-#while (icmpPacket[ICMP].type == 11):
-#	#print(icmpPacket[ICMP].src)
-#	print(timeToLive)
-#	print(icmpPacket[IP].src)
-#	ip = IP(dst=target, ttl=timeToLive)
-#	icmpPacket = sr1(ip/icmp/"XXXXXXXX")
-#	#icmpPacket.show()
-#	timeToLive+=1
-#	
-#print(icmpPacket[ICMP].src)
