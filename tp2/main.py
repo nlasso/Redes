06fca97e24e6f61ab8884b71a0ee19343c0d5a9c
Nicolás Lasso <lasso.nico@gmail.com>
2015-06-08 23:25:46 -0300

    Agregado mediciones del RTT. Falta confirmar con los profesores.

--- tp2/main.py
+++ tp2/main.py
@@ -3,22 +3,29 @@
 import sys
 import signal
 import pickle
+import time, math
 from scapy.all import *
 
 def main():
 	timeToLive = 1
 	target = "www.facebook.com"
-
+	rtts = []
 	while True:
 		pass
 		icmp = ICMP()
 		icmp.type=8
 		pkt = IP(dst=target, ttl=timeToLive)/icmp/"XXXX"
+		startTime = time.time()
 		icmpPacket = sr1(pkt, verbose=0, timeout=2)
+		endTime = time.time()
 		if (not (icmpPacket is None)):
-			icmpPacket[0].display()
+			#icmpPacket[0].display()
 			if (icmpPacket[ICMP].type == 11):
 				print("Time Exceeded en la IP: ", icmpPacket[IP].src)
+				rttTotal = endTime - startTime
+				rttTotal *= 1000
+				rttDec, rtt = math.modf(rttTotal)
+				print("El RTT para esta IP", icmpPacket[IP].src, " fue de: ", rtt)
 			else:
 				print("Llegaste hasta", target, " que tiene la IP: ", icmpPacket[IP].src)
 				break
