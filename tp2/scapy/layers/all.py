bdf86576f94287db048e8ad4cdfb5f77e3c485f9
ramaroberto <bertoski@gmail.com>
2015-06-13 14:22:16 -0300

    Removido warning molesto

--- tp2/scapy/layers/all.py
+++ tp2/scapy/layers/all.py
@@ -13,7 +13,7 @@ from scapy.error import log_loading
 import logging
 log = logging.getLogger("scapy.loading")
 
-log.warning("Please, report issues to https://github.com/phaethon/scapy")
+#log.warning("Please, report issues to https://github.com/phaethon/scapy")
 
 def _import_star(m):
     #mod = __import__("." + m, globals(), locals())
