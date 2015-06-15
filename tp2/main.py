9af9d31d81dad7b038f87a016de4922d388d22e1
Pablo Somodi <psomodi@dc.uba.ar>
2015-06-15 17:47:59 -0300

    refactor Laski

--- tp2/main.py
+++ tp2/main.py
@@ -5,78 +5,72 @@ import signal
 import pickle
 import time, math
 import logging
+from socket import gethostbyname
+from statistics import mean, stdev
+from scapy.all import *
 
 logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
-from scapy.all import *
-from statistics import mean, stdev
 
-def main(target, reps):
-	ECHO_REQUEST_TYPE = 8
-	TTL_EXPIRED_IN_TRANSIT = 11
+ECHO_REQUEST_TYPE = 8
+TTL_EXPIRED_IN_TRANSIT = 11
+
+def get_rtt(target, ttl):
+	icmp = ICMP()
+	icmp.type= ECHO_REQUEST_TYPE
+	pkt = IP(dst=target, ttl=ttl)/icmp/"X"
+	startTime = time.time()
+	response = sr1(pkt, verbose=0, timeout=2)
+	endTime = time.time()
+	rttTotal = (endTime - startTime) * 1000
+	rttDec, rtt = math.modf(rttTotal)
+	return response, rtt
+
+def explore_jumps(target_txt):
+	target = gethostbyname(target_txt)
 	timeToLive = 1
 	rtts = []
 	jumps = []
 	ttls = []
-
-	icmp = ICMP()
-	icmp.type= ECHO_REQUEST_TYPE
-
-	# Exploro los saltos
-	while True:
-		# Preparo paquete y mido tiempo
-		pkt = IP(dst=target, ttl=timeToLive)/icmp/"X"
-		startTime = time.time()
-		icmpPacket = sr1(pkt, verbose=0, timeout=2)
-		endTime = time.time()
+	while timeToLive < 35:
+		response, rtt = get_rtt(target, timeToLive)
 		print("TTL=", timeToLive)
-		if (not (icmpPacket is None)):
-			# Calculo RTT
-			rttTotal = (endTime - startTime) * 1000
-			rttDec, rtt = math.modf(rttTotal)
+		if not response is None:
 			rtts.append(rtt)
-			jumps.append(icmpPacket[IP].src)
+			jumps.append(response[IP].src)
 			ttls.append(timeToLive)
-			if (icmpPacket[ICMP].type == TTL_EXPIRED_IN_TRANSIT):
-				print("Time Exceeded en la IP: ", icmpPacket[IP].src)
-				print("El RTT para esta IP", icmpPacket[IP].src, " fue de: ", rtt)
+			if response[ICMP].type == TTL_EXPIRED_IN_TRANSIT:
+				print("El RTT para esta IP", response[IP].src, "fue de:", rtt)
 			else:
-				print("Llegaste hasta", target, " que tiene la IP: ", icmpPacket[IP].src)
+				print("Llegaste hasta", target_txt, "que tiene la IP:", response[IP].src)
 				break
 		timeToLive += 1
+	if timeToLive == 35:
+		print("ERROR: El host" + target + "es inalcanzable")
+	return rtts, jumps, ttls
 
-		if timeToLive > 35:
-			print("ERROR: El host " + target + " es inalcanzable")
-			exit()
+
+def main(target, reps):
+	timeToLive = 1
+
+	rtts, jumps, ttls = explore_jumps(target)
 
 	# Mido ZRTT
 	avg_rtts = rtts
 	total_rtts = [rtts]
 	rtts = []
 
-	for rep in range(1,reps):
-		print ("Ejecutando repeticion " + str(rep) + "...")
+	for rep in range(1, reps):
+		print ("Ejecutando repeticion", str(rep), "...")
 
-		for i in range(0,len(jumps)):
-
-			# Preparo paquete y mido tiempo
-			pkt = IP(dst=jumps[i], ttl=ttls[i])/icmp/"X"
-			startTime = time.time()
-			icmpPacket = sr1(pkt, verbose=0, timeout=2)
-			endTime = time.time()
-			
-			# Calculo RTT
-			rttTotal = (endTime - startTime) * 1000
-			rttDec, rtt = math.modf(rttTotal)
+		for i in range(0, len(jumps)):
+			response, rtt = get_rtt(jumps[i], ttls[i])
 			rtts.append(rtt)
 
 			# Sumo RTT
-			#print(jumps[i])
-			#print(avg_rtts[i])
 			avg_rtts[i] += rtt
-			#print(rtt)
-			#print(avg_rtts[i])
 		total_rtts.append(rtts)
 
+	# Promedio
 	for i in range(0, len(avg_rtts)):
 		avg_rtts[i] /= reps
 
@@ -85,8 +79,7 @@ def main(target, reps):
 		zrtt = (avg_rtts[i]-mean(avg_rtts))/stdev(avg_rtts)
 		zrtts.append(zrtt)
 
-	#print(len(zrtts), len(avg_rtts), len(jumps))
-	return (zrtts, avg_rtts, total_rtts, jumps)
+	return zrtts, avg_rtts, total_rtts, jumps
 
 
 
@@ -101,11 +94,9 @@ if __name__ == "__main__":
 	else:
 		reps = int(sys.argv[2])
 
-	(zrtts, avg_rtts, total_rtts, jumps) = main(sys.argv[1], reps)
+	zrtts, avg_rtts, total_rtts, jumps = main(sys.argv[1], reps)
 
-	print(zrtts)
-	print(avg_rtts)
-	print(total_rtts)
-	print(jumps)
-
-
+	print("ZRTTS:", zrtts)
+	print("AVG RTTS:", avg_rtts)
+	print("TOTAL RTTS:", total_rtts)
+	print("JUMPS:", jumps)
