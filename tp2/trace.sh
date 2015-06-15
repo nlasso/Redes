9768ec24771de7c910f75e7258d22e99c70f70ae
ramaroberto <bertoski@gmail.com>
2015-06-15 01:00:17 -0300

    Chequeadas las distintas direcciones

--- tp2/trace.sh
+++ tp2/trace.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-./main www.facebook.com
-./main www.waikato.ac.nz # Universidad de Waikato (Pais al Este de Australia)
-./main en.psu.ru # Perm State University -- Universidad en Perm, Rusia
-./main www.univ-antananarivo.mg # Universidad de Madagascar (Este de Africa)
-./main english.hi.is # Iceland University
-./main www.uaf.edu # Universidad de Alaska
+./main.py www.facebook.com
+##./main.py www.waikato.ac.nz # Universidad de Waikato (Pais al Este de Australia)
+./main.py en.psu.ru # Perm State University -- Universidad en Perm, Rusia
+./main.py www.univ-antananarivo.mg # Universidad de Madagascar (Este de Africa)
+./main.py english.hi.is # Iceland University
+##./main.py www.uaf.edu # Universidad de Alaska
