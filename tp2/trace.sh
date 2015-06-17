2d7ab8839ebff0a6458e0682a25a97cdedd2800f
ramaroberto <bertoski@gmail.com>
2015-06-16 21:28:17 -0300

    Script para realizar tests

--- tp2/trace.sh
+++ tp2/trace.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
 
 ./main.py berkeley.edu 50 > results/berkeley.dat # Universidad de Berkeley, California, EEUU
+./main.py test berkeley.edu > results/berkeley_throughput.dat # Universidad de Berkeley, California, EEUU
 echo "Completado berkley.edu"
 
-./main.py en.psu.ru 50 > esults/psu.dat # Perm State University -- Universidad en Perm, Rusia
+./main.py en.psu.ru 50 > results/psu.dat # Perm State University -- Universidad en Perm, Rusia
+./main.py test en.psu.ru > results/psu_throughput.dat
 echo "Completado en.psu.ru"
 
 ./main.py www.univ-antananarivo.mg 50 > results/antananarivo.dat # Universidad de Madagascar (Este de Africa)
+./main.py test www.univ-antananarivo.mg > results/antananarivo_throughput.dat
 echo "Completado antananarivo (que nombre!)"
 
 ./main.py english.hi.is 50 > results/hi.dat # Iceland University
+./main.py test english.hi.is > results/hi_throughput.dat
 echo "Completado Islandia"
 
 ./main.py www.mu.ac.in 50 > results/muac.dat # Universidad de Bombay (India)
+./main.py test www.mu.ac.in > results/muac_throughput.dat
 echo "Completado mu.ac (Bombay - India)"
 
 ./main.py www.u-tokyo.ac.jp 50 > results/tokyo.dat # Universidad de Tokyo, Japón
+./main.py test www.u-tokyo.ac.jp > results/tokyo_throughput.dat
 echo "Compeltado Tokyo"
